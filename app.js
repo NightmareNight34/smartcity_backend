36ced13317856301e7f60084bfc554d139dcc759
Nightmare_Night <samik2160@yandex.ru>
2023-04-24 20:21:16 +0300

    modified:   app.js 	modified:   routes/user.js 	deleted:    users.sql 	modified:   views/signup.ejs

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
-/**
-* Module dependencies.
-*/
+
 const express = require('express')
   , routes = require('./routes')
   , user = require('./routes/user')
   , http = require('http')
   , path = require('path');
-//const methodOverride = require('method-override');
+
 const session = require('express-session');
 const app = express();
 const mysql      = require('mysql');
@@ -22,7 +20,7 @@ connection.connect();
  
 global.db = connection;
  
-// all environments
+
 app.set('port', process.env.PORT || 8080);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
@@ -30,7 +28,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static('public'))
 
-// app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(session({
               secret: 'keyboard cat',
               resave: false,
@@ -38,7 +36,7 @@ app.use(session({
               cookie: { maxAge: 60000 }
             }))
  
-// development only
+
  
 app.get('/', routes.index);//call for main index page
 app.get('/signup', user.signup);//call for signup page
@@ -48,5 +46,5 @@ app.post('/login', user.login);//call for login post
 app.get('/home/dashboard', user.dashboard);//call for dashboard page after login
 app.get('/home/logout', user.logout);//call for logout
 app.get('/home/profile',user.profile);//to render users profile
-//Middleware
+
 app.listen(8080)
